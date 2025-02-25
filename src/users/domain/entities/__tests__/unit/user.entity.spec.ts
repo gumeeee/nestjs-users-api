235a95b0fc4ef934c7b8dbd80c79ff11b463a340
gumeeee <gaxco123@gmail.com>
2025-02-24 21:26:28 -0300

    feat: UserEntity - getters methods this fields unit tests

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -21,4 +21,27 @@ describe('User Entity', () => {
     expect(sut.props.password).toEqual(props.password);
     expect(sut.props.created_at).toBeInstanceOf(Date);
   });
+
+  it('Getter of name field', () => {
+    expect(sut.props.name).toBeDefined();
+    expect(sut.props.name).toEqual(props.name);
+    expect(typeof sut.props.name).toBe('string');
+  });
+
+  it('Getter of email field', () => {
+    expect(sut.props.email).toBeDefined();
+    expect(sut.props.email).toEqual(props.email);
+    expect(typeof sut.props.email).toBe('string');
+  });
+
+  it('Getter of password field', () => {
+    expect(sut.props.password).toBeDefined();
+    expect(sut.props.password).toEqual(props.password);
+    expect(typeof sut.props.password).toBe('string');
+  });
+
+  it('Getter of created_at field', () => {
+    expect(sut.props.created_at).toBeDefined();
+    expect(sut.props.created_at).toBeInstanceOf(Date);
+  });
 });
