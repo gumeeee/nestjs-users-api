fb48a03b61a2815ca0c497f8f4f3dd879c8a11b4
gumeeee <gaxco123@gmail.com>
2025-03-21 21:54:12 -0300

    feat: adding search methods in InMemorySearchableRepository

--- src/shared/domain/repositories/in-memory-searchable.repository.ts
+++ src/shared/domain/repositories/in-memory-searchable.repository.ts
@@ -1,13 +1,39 @@
 import { Entity } from '../entity/entity';
 import { inMemoryRepository } from './in-memory.repository';
-import { SearchableRepositoryInterface } from './searchable-repository-contracts';
+import {
+  SearchableRepositoryInterface,
+  SearchParams,
+  SearchResult,
+} from './searchable-repository-contracts';
 
 export abstract class inMemorySearchableRepository<E extends Entity>
   extends inMemoryRepository<E>
   implements SearchableRepositoryInterface<E, any, any>
 {
-  search(props: any): Promise<any> {
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async search(props: SearchParams): Promise<SearchResult<E>> {
     throw new Error('Method not implemented.');
+  }
+
+  protected abstract applyFilter(
+    items: E[],
+    filter: string | null,
+  ): Promise<E[]>;
+
+  protected async applySort(
+    items: E[],
+    sort: string | null,
+    sortDirection: string | null,
+  ): Promise<E[]> {
+    return Promise.resolve([]);
+  }
+
+  protected async applyPagination(
+    items: E[],
+    page: SearchParams['page'],
+    pageSize: SearchParams['pageSize'],
+  ): Promise<E[]> {
+    return Promise.resolve([]);
   }
 
   items: E[] = [];
