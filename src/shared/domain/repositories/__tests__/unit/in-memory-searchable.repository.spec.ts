1380e9a11fb80c8713ece9cdff1cd61d7040a9c8
gumeeee <gaxco123@gmail.com>
2025-03-24 20:52:05 -0300

    feat: Adding unit tests InMemorySearchableRepository class

---
+++ src/shared/domain/repositories/__tests__/unit/in-memory-searchable.repository.spec.ts
@@ -0,0 +1,45 @@
+import { Entity } from '@/shared/domain/entity/entity';
+import { inMemorySearchableRepository } from '../../in-memory-searchable.repository';
+
+type StubEntityProps = {
+  name: string;
+  price: number;
+};
+
+class StubEntity extends Entity<StubEntityProps> {}
+
+class StubInMemorySearchableRepository extends inMemorySearchableRepository<StubEntity> {
+  sortableFields: string[] = ['name'];
+
+  // eslint-disable-next-line @typescript-eslint/require-await
+  protected async applyFilter(
+    items: StubEntity[],
+    filter: string | null,
+  ): Promise<StubEntity[]> {
+    if (!filter) {
+      return items;
+    }
+
+    return items.filter(item =>
+      item.props.name.toLowerCase().includes(filter.toLowerCase()),
+    );
+  }
+}
+
+describe('InMemoryRepository unit tests', () => {
+  let sut: StubInMemorySearchableRepository;
+
+  beforeEach(() => {
+    sut = new StubInMemorySearchableRepository();
+  });
+
+  describe('applyFilter method', () => {
+    it('', async () => {});
+  });
+
+  describe('applySort method', () => {});
+
+  describe('applyPagination method', () => {});
+
+  describe('search method', () => {});
+});
