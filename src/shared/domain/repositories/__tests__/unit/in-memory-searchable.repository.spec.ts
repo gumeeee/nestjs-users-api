ba65bc44c4d577eb316164becf75e16914be722d
gumeeee <gaxco123@gmail.com>
2025-03-25 00:50:57 -0300

    fix: change a Repository name

--- src/shared/domain/repositories/__tests__/unit/in-memory-searchable.repository.spec.ts
+++ src/shared/domain/repositories/__tests__/unit/in-memory-searchable.repository.spec.ts
@@ -26,7 +26,7 @@ class StubInMemorySearchableRepository extends inMemorySearchableRepository<Stub
   }
 }
 
-describe('InMemoryRepository unit tests', () => {
+describe('InMemorySearchableRepository unit tests', () => {
   let sut: StubInMemorySearchableRepository;
 
   beforeEach(() => {
