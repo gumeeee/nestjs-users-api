696251027f34af90e1c0e5e4f3475ee3aa937dc3
gumeeee <gaxco123@gmail.com>
2025-03-30 00:58:22 -0300

    feat: Adding unit test in UserInMemoryRepository - applyFilter Method, and refactor it name in emailExists method

--- src/users/infrastructure/database/in-memory/repositories/__tests__/unit/user-in-memory.repository.spec.ts
+++ src/users/infrastructure/database/in-memory/repositories/__tests__/unit/user-in-memory.repository.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/await-thenable */
 import { UserEntity } from '@/users/domain/entities/user.entity';
 import { UserInMemoryRepository } from '../../user-in-memory.repository';
@@ -12,31 +13,62 @@ describe('UserInMemoryRepository unit tests', () => {
     sut = new UserInMemoryRepository();
   });
 
-  it('Should throw error when not found - findByEmail method', async () => {
-    await expect(sut.findByEmail('test@email.com')).rejects.toThrow(
-      new NotFoundError('Entity not found using email: test@email.com'),
-    );
+  describe('findByEmail method', () => {
+    it('Should throw error when not found', async () => {
+      await expect(sut.findByEmail('test@email.com')).rejects.toThrow(
+        new NotFoundError('Entity not found using email: test@email.com'),
+      );
+    });
+
+    it('Should find a entity by email', async () => {
+      const userEntity = new UserEntity(UserDataBuilder({}));
+      await sut.insert(userEntity);
+      const foundEntity = await sut.findByEmail(userEntity.email);
+
+      expect(foundEntity.toJSON()).toStrictEqual(userEntity.toJSON());
+    });
   });
 
-  it('Should find a entity by email - findByEmail method', async () => {
-    const userEntity = new UserEntity(UserDataBuilder({}));
-    await sut.insert(userEntity);
-    const foundEntity = await sut.findByEmail(userEntity.email);
+  describe('emailExists method', () => {
+    it('Should throw error when conflict', async () => {
+      const userEntity = new UserEntity(UserDataBuilder({}));
+      await sut.insert(userEntity);
 
-    expect(foundEntity.toJSON()).toStrictEqual(userEntity.toJSON());
+      await expect(sut.emailExists(userEntity.email)).rejects.toThrow(
+        new ConflictError('Email adress already used by another user'),
+      );
+    });
+
+    it('Should not throw error when not conflict', async () => {
+      expect.assertions(0);
+      await sut.emailExists('test@email.com');
+    });
   });
 
-  it('Should throw error when conflict - emailExists method', async () => {
-    const userEntity = new UserEntity(UserDataBuilder({}));
-    await sut.insert(userEntity);
+  describe('applyFilter method', () => {
+    it('Should no filter items when filter object is null', async () => {
+      const userEntity = new UserEntity(UserDataBuilder({}));
+      await sut.insert(userEntity);
+      const result = await sut.findAll();
+      const spyFilter = jest.spyOn(result, 'filter');
+      const itemsFiltered = await sut['applyFilter'](result, null as any);
 
-    await expect(sut.emailExists(userEntity.email)).rejects.toThrow(
-      new ConflictError('Email adress already used by another user'),
-    );
-  });
+      expect(spyFilter).not.toHaveBeenCalled();
+      expect(itemsFiltered).toStrictEqual(result);
+    });
 
-  it('Should throw error when conflict - emailExists method', async () => {
-    expect.assertions(0);
-    await sut.emailExists('test@email.com');
+    it('Should filter name field using filter param', async () => {
+      const items = [
+        new UserEntity(UserDataBuilder({ name: 'test' })),
+        new UserEntity(UserDataBuilder({ name: 'fake name' })),
+        new UserEntity(UserDataBuilder({ name: 'TEST' })),
+        new UserEntity(UserDataBuilder({ name: 'Test' })),
+      ];
+      const spyFilter = jest.spyOn(items, 'filter');
+      const itemsFiltered = await sut['applyFilter'](items, 'TEST');
+
+      expect(spyFilter).toHaveBeenCalled();
+      expect(itemsFiltered).toStrictEqual([items[0], items[2], items[3]]);
+    });
   });
 });
