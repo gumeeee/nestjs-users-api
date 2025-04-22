8e6ee69f4ece5069f822c13de184066050e71faa
gumeeee <gaxco123@gmail.com>
2025-04-22 02:10:00 -0300

    feat: add UsersController unit tests - remove method

--- src/users/infrastructure/__tests__/unit/users.controller.spec.ts
+++ src/users/infrastructure/__tests__/unit/users.controller.spec.ts
@@ -112,4 +112,21 @@ describe('UsersController unit tests', () => {
       ...input,
     });
   });
+
+  it('should delete a user password', async () => {
+    const output = undefined;
+    const mockDeleteUserUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(output)),
+    };
+
+    sut['deleteUserUseCase'] = mockDeleteUserUseCase as any;
+
+    const result = await sut.remove(id);
+
+    expect(output).toStrictEqual(result);
+    expect(mockDeleteUserUseCase.execute).toHaveBeenCalledTimes(1);
+    expect(mockDeleteUserUseCase.execute).toHaveBeenCalledWith({
+      id,
+    });
+  });
 });
