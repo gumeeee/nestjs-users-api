b42ce414ce7dd6e20d182dc590d2ad5f2773ee89
gumeeee <gaxco123@gmail.com>
2025-05-21 00:55:30 -0300

    feat: adding UserCollectionPresenter unit tests

--- src/users/infrastructure/presenters/__tests__/unit/user.presenter.spec.ts
+++ src/users/infrastructure/presenters/__tests__/unit/user.presenter.spec.ts
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { UserOutput } from '@/users/application/dtos/user-output';
 import { instanceToPlain } from 'class-transformer';
-import { UserPresenter } from '../../user.presenter';
+import { UserCollectionPresenter, UserPresenter } from '../../user.presenter';
+import { PaginationPresenter } from '@/shared/infrastructure/presenters/pagination.presenter';
 
 describe('UserPresenter unit tests', () => {
   const created_at = new Date();
@@ -33,6 +35,96 @@ describe('UserPresenter unit tests', () => {
         name: 'Test name',
         email: 'test@email.com',
         created_at: created_at.toISOString(),
+      });
+    });
+  });
+});
+
+describe('UserCollectionPresenter unit tests', () => {
+  const created_at = new Date();
+  const props: UserOutput = {
+    id: '733a52a8-fccf-44aa-976c-fda7b8fe1697',
+    name: 'Test name',
+    password: 'test@password',
+    email: 'test@email.com',
+    created_at,
+  };
+
+  describe('constructor method', () => {
+    it('should be defined', () => {
+      const sut = new UserCollectionPresenter({
+        items: [props],
+        currentPage: 1,
+        pageSize: 1,
+        lastPage: 1,
+        total: 1,
+      });
+
+      expect(sut.meta).toBeInstanceOf(PaginationPresenter);
+      expect(sut.meta).toStrictEqual(
+        new PaginationPresenter({
+          currentPage: 1,
+          pageSize: 1,
+          lastPage: 1,
+          total: 1,
+        }),
+      );
+      expect(sut.data).toStrictEqual([new UserPresenter(props)]);
+    });
+
+    it('should presenter data', () => {
+      let sut = new UserCollectionPresenter({
+        items: [props],
+        currentPage: 1,
+        pageSize: 2,
+        lastPage: 1,
+        total: 1,
+      });
+
+      let output = instanceToPlain(sut);
+
+      expect(output).toStrictEqual({
+        data: [
+          {
+            id: '733a52a8-fccf-44aa-976c-fda7b8fe1697',
+            name: 'Test name',
+            email: 'test@email.com',
+            created_at: created_at.toISOString(),
+          },
+        ],
+        meta: {
+          currentPage: 1,
+          pageSize: 2,
+          lastPage: 1,
+          total: 1,
+        },
+      });
+
+      sut = new UserCollectionPresenter({
+        items: [props],
+        currentPage: '1' as any,
+        pageSize: '2' as any,
+        lastPage: '1' as any,
+        total: '1' as any,
+      });
+
+      output = instanceToPlain(sut);
+
+      expect(output).toStrictEqual({
+        data: [
+          {
+            id: '733a52a8-fccf-44aa-976c-fda7b8fe1697',
+            name: 'Test name',
+            email: 'test@email.com',
+            created_at: created_at.toISOString(),
+          },
+        ],
+        meta: {
+          currentPage: 1,
+          pageSize: 2,
+          lastPage: 1,
+          total: 1,
+        },
       });
     });
   });
