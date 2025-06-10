8025e5a0c2acf537efae0311d04a487f25997886
gumeeee <gaxco123@gmail.com>
2025-06-09 23:02:45 -0300

    refactor: remove console log in WrapperDataInterceptor

--- src/shared/infrastructure/interceptors/wrapper-data/wrapper-data.interceptor.ts
+++ src/shared/infrastructure/interceptors/wrapper-data/wrapper-data.interceptor.ts
@@ -11,8 +11,6 @@ export class WrapperDataInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       map(body => {
-        console.log('Intercepted ', body);
-
         // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-assignment
         return !body || 'meta' in body ? body : { data: body };
       }),
