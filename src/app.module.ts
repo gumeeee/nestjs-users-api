534913c0e06e93320f24b3418665d522625317f2
gumeeee <gaxco123@gmail.com>
2025-06-14 19:49:14 -0300

    chore: implementing AuthModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './shared/infrastructure/database/database.module';
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module';
 import { UsersModule } from './users/infrastructure/users.module';
+import { AuthModule } from './auth/infrastructure/auth.module';
 
 @Module({
-  imports: [EnvConfigModule, UsersModule, DatabaseModule],
+  imports: [EnvConfigModule, UsersModule, DatabaseModule, AuthModule],
 })
 export class AppModule {}
