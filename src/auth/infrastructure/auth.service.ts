bbcf5318af6b2e982a8ff4924b1bbd6c2d3b87ec
gumeeee <gaxco123@gmail.com>
2025-06-14 20:06:15 -0300

    chore: implementing AuthService

--- src/auth/infrastructure/auth.service.ts
+++ src/auth/infrastructure/auth.service.ts
@@ -1,4 +1,28 @@
+import { EnvConfigService } from '@/shared/infrastructure/env-config/env-config.service';
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+type GenerateJwtProps = {
+  acessToken: string;
+};
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private jwtService: JwtService,
+    private configService: EnvConfigService,
+  ) {}
+
+  async generateJwt(userId: string): Promise<GenerateJwtProps> {
+    const acessToken = await this.jwtService.signAsync({ id: userId }, {});
+
+    return { acessToken };
+  }
+
+  async verifyJwt(token: string) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return this.jwtService.verifyAsync(token, {
+      secret: this.configService.getJwtSecret(),
+    });
+  }
+}
