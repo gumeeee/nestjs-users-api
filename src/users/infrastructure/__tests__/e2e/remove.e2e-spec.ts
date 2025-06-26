890578302291e3fe0fe9034360e203d619d075e8
gumeeee <gaxco123@gmail.com>
2025-06-25 22:05:13 -0300

    refactor: Refactoring e2e tests of the remove method

--- src/users/infrastructure/__tests__/e2e/remove.e2e-spec.ts
+++ src/users/infrastructure/__tests__/e2e/remove.e2e-spec.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { applyGlobalConfig } from '@/global-config';
+import { HashProvider } from '@/shared/application/providers/hash-provider';
 import { DatabaseModule } from '@/shared/infrastructure/database/database.module';
 import { setupPrismaTests } from '@/shared/infrastructure/database/prisma/testing/setup-prisma-tests';
 import { EnvConfigModule } from '@/shared/infrastructure/env-config/env-config.module';
@@ -9,10 +10,9 @@ import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builde
 import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaClient } from '@prisma/client';
-import { instanceToPlain } from 'class-transformer';
 import request from 'supertest';
-import { UsersController } from '../../users.controller';
 import { UsersModule } from '../../users.module';
+import { BcryptjsHashProvider } from '../../providers/hash-provider/bcryptjs-hash.provider';
 
 describe('UsersController e2e tests', () => {
   let app: INestApplication;
@@ -20,6 +20,9 @@ describe('UsersController e2e tests', () => {
   let repository: UserRepository.Repository;
   const prismaService = new PrismaClient();
   let entity: UserEntity;
+  let hashProvider: HashProvider;
+  let hashPassword: string;
+  let accessToken: string;
 
   beforeAll(async () => {
     setupPrismaTests();
@@ -34,18 +37,38 @@ describe('UsersController e2e tests', () => {
     applyGlobalConfig(app);
     await app.init();
     repository = module.get<UserRepository.Repository>('UserRepository');
+    hashProvider = new BcryptjsHashProvider();
+    hashPassword = await hashProvider.generateHash('123456');
   });
 
   beforeEach(async () => {
     await prismaService.user.deleteMany({});
-    entity = new UserEntity(UserDataBuilder({}));
+    entity = new UserEntity(
+      UserDataBuilder({
+        email: 'test@email.com',
+        password: hashPassword,
+      }),
+    );
     await repository.insert(entity);
+
+    const loginResponse: { body: { accessToken: string } } = await request(
+      app.getHttpServer(),
+    )
+      .post('/users/login')
+      .send({
+        email: 'test@email.com',
+        password: '123456',
+      })
+      .expect(200);
+
+    accessToken = loginResponse.body.accessToken;
   });
 
   describe('DELETE /users/:id', () => {
     it('should remove a user', async () => {
       const res = await request(app.getHttpServer())
         .delete(`/users/${entity.id}`)
+        .set('Authorization', `Bearer ${accessToken}`)
         .expect(204)
         .expect({});
     });
@@ -53,11 +76,23 @@ describe('UsersController e2e tests', () => {
     it('should return a erro with 404 code when throw NotFoundError with invalid id', async () => {
       const res = await request(app.getHttpServer())
         .delete('/users/fakeid')
+        .set('Authorization', `Bearer ${accessToken}`)
         .expect(404)
         .expect({
           statusCode: 404,
           error: 'Not Found',
           message: 'UserModel not found using ID: fakeid',
+        });
+    });
+
+    it('should return a erro with 401 code when the request is not authorized', async () => {
+      const res = await request(app.getHttpServer())
+        .delete('/users/fakeid')
+        .expect(401)
+        .expect({
+          statusCode: 401,
+          error: 'Unauthorized',
+          message: 'No token provided',
         });
     });
   });
