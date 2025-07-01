7df3cce897500dde557c7259a6e1c55b87355a40
gumeeee <gaxco123@gmail.com>
2025-07-01 02:17:17 -0300

    chore: adding the documentation in controller findOne method

--- src/users/infrastructure/users.controller.ts
+++ src/users/infrastructure/users.controller.ts
@@ -163,6 +163,15 @@ export class UsersController {
     return UsersController.listUsersToResponse(output);
   }
 
+  @ApiBearerAuth()
+  @ApiResponse({
+    status: 404,
+    description: 'Id não encontrado.',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Acesso não autorizado.',
+  })
   @UseGuards(AuthGuard)
   @Get(':id')
   async findOne(@Param('id') id: string) {
